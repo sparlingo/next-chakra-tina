5a8f828336dc8d706d463bf2407838e84d72568d
Kevin Sparling <kevinsparling@gmail.com>
2022-08-06 13:23:39 +0800

    working on sidebar

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,11 @@
-import {
-  AiOutlineMenu,
-  Box,
-  Button,
-  chakra,
-  Flex,
-  Heading,
-  IconButton,
-  Stack,
-} from '@chakra-ui/react'
-//import Header from '../components/Header'
-//import Hero from '../components/Hero'
+
 import {Footer} from '../components/Footer'
+import {Sidebar} from '../components/Sidebar'
 
 export default function Home() {
   return (
     <div>
-        
+      {/* <Sidebar /> */}
       <Footer />
     </div>
   )
