249c8a48faa58828d9a4dcca950c65fd91603e60
Kevin Sparling <kevinsparling@gmail.com>
2022-08-07 17:21:58 +0800

    navbar works for now, added hero placeholder

--- components/Header.js
+++ components/Header.js
@@ -20,7 +20,7 @@ const CloseIcon = () => (
   <svg width="24" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
     <title>Close</title>
     <path
-      fill="white"
+      fill="black"
       d="M9.00023 7.58599L13.9502 2.63599L15.3642 4.04999L10.4142 8.99999L15.3642 13.95L13.9502 15.364L9.00023 10.414L4.05023 15.364L2.63623 13.95L7.58623 8.99999L2.63623 4.04999L4.05023 2.63599L9.00023 7.58599Z"
     />
   </svg>
@@ -31,7 +31,7 @@ const MenuIcon = () => (
     width="24px"
     viewBox="0 0 20 20"
     xmlns="http://www.w3.org/2000/svg"
-    fill="white"
+    fill="black"
   >
     <title>Menu</title>
     <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
@@ -49,8 +49,8 @@ const Header = (props) => {
       justify="space-between"
       wrap="wrap"
       w="100%"
-      mb={8}
-      p={8}
+      mb={2}
+      p={7}
       bg={["grey.500", "primary.500", "transparent", "transparent"]}
       color={["grey.800", "grey.100", "primary.700", "primary.700"]}
       {...props}
@@ -84,8 +84,9 @@ const Header = (props) => {
             <Button
               size="sm"
               rounded="md"
+
               color={["primary.500", "primary.500", "white", "white"]}
-              bg={["black", "black", "primary.500", "primary.500"]}
+              bg={["red.500", "red.500", "primary.500", "primary.500"]}
               _hover={{
                 bg: ["primary.100", "primary.100", "primary.600", "primary.600"]
               }}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Hero />
       <Footer />
     </div>
   )
